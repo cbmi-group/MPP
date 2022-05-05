1b18696b5b35ac93c243471a15e888de958c5a3b
sissel <84493698+24yearsoldstudent@users.noreply.github.com>
2022-05-05 20:47:02 +0800

    Delete MPP-master directory

--- MPP-master/run.sh
+++
@@ -1,8 +0,0 @@
-python ./dataset/_augment.py --stage surpevised
-python train.py --stage supervised --epoch 100
-python inference.py --stage supervised
-python image_monography.py
-python ./dataset/_augment.py --stage semi-surpevised
-python train.py --stage semi-supervised --epoch 100
-python test_Unet.py
-python test_space_Unet.py
